0101a6d86709f3d561243cb17596746d6b134dd3
Seth <sethboyle2002@gmail.com>
2020-01-06 23:49:28 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -4,15 +4,13 @@ return "You are a teenager!"
 }
 }
 
-
-
-
-
-
-
-
-
 function teenager(age) {
+  if(age >= 13 && age <= 19) {
+  return "You are a teenager!"
+  }
+  else {
+    return "You are not a teenager!"
+  }
 
 }
 
