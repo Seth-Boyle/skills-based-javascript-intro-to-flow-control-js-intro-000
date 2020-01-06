883be896ffd41a9e1c5117eda3b08347d8f41880
Seth <sethboyle2002@gmail.com>
2020-01-06 23:13:52 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,6 +1,6 @@
 function basicTeenager(age) {
 if (age >= 13 && age <= 19) {
-return console.log ("You are a teenager!");
+return console.log ('You are a teenager!');
 }
 else {
   return console.log ("You are not a teenager!");
