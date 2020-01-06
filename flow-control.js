fd5c1f9ca3bdf5d04568d71c178dabfd17dfaa2e
Seth <sethboyle2002@gmail.com>
2020-01-06 23:12:28 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,13 +1,26 @@
 function basicTeenager(age) {
-
+if (age >= 13 && age <= 19) {
+return console.log ("You are a teenager!")
+}
+else {
+  return console.log ("You are not a teenager!")
+}
 }
 
 function teenager(age) {
-
+  if (age >= 13 && age <= 19) {
+  return console.log ("You are a teenager!")
+  }
+  else {
+    return console.log ("You are not a teenager!")
+  }
 }
 
 function ageChecker(age) {
+  if (age < 13 || age > 19){
+  return console.log ("You are not a teenager!")
 
+}
 }
 
 function ternaryTeenager(age) {
@@ -15,5 +28,9 @@ function ternaryTeenager(age) {
 }
 
 function switchAge(age) {
+  if (age < 13 || age > 19){
+return console.log ("You have an age")
+
+}
 
 }
