25e3432df6fbcfd42439c1d6861bb62f167eba43
Seth <sethboyle2002@gmail.com>
2020-01-07 00:31:39 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -28,33 +28,11 @@ function ageChecker(age) {
 
  function ternaryTeenager(age) {
 
-age >= 13 && age <= 19 ? console.log "You are a teenager!" : console.log "You are not a teenager"
+
 }
 
 
 
  function switchAge(age) {
-case 13:
- console.log("You are a teenager")
-break;
-case 14 :
- console.log("You are a teenager")
-break;
-case 15 :
- console.log("You are a teenager")
-break;
-case 16 :
- console.log("You are a teenager")
-break;
-case 17 :
- console.log("You are a teenager")
-break;
-case 18 :
- console.log("You are a teenager")
-break;
-case 19 :
- console.log("You are a teenager")
-break;
-default: console.log("You have an age")
 
 }
